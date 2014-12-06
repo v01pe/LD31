4cce4665fec66dfb80548d01745778290daa5a98
Jan Hackl <jan@brokenrul.es>
2014-12-06 22:20:53 +0100

    control component

---
+++ Intersect/Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public static float speed = 10.0f;
+
+	public int nr;
+
+	private Vector2 inputVector;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		inputVector.x = Input.GetAxis("Horizontal P" + nr);
+		inputVector.y = -Input.GetAxis("Vertical P" + nr);
+
+		gameObject.rigidbody2D.AddForce(inputVector * speed);
+	}
+}
