218f7171ac24ac8cbae733ddc0107c590d8dd69d
Jan Hackl <jan@brokenrul.es>
2014-12-06 22:46:23 +0100

    ball + basic collision

---
+++ Intersect/Assets/GameState.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour {
+
+	public Player thrower;
+
+	void Start ()
+	{
+	
+	}
+
+	void Update ()
+	{
+		
+	}
+}
--- Intersect/Assets/Player.cs
+++ Intersect/Assets/Player.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	public static float speed = 10.0f;
+	public static float speed = 3.0f;
 
 	public int nr;
 
@@ -22,6 +22,19 @@ public class Player : MonoBehaviour {
 		inputVector.x = Input.GetAxis("Horizontal P" + nr);
 		inputVector.y = -Input.GetAxis("Vertical P" + nr);
 
-		gameObject.rigidbody2D.AddForce(inputVector * speed);
+//		print("x " + inputVector.x + " y " + inputVector.y);
+
+		Vector2 targetVelocity = (inputVector.magnitude == 0) ? Vector2.zero : inputVector.normalized * speed;
+
+//		Vector2 currentVelocity = rigidbody2D.velocity;
+//		float currentSpeed = currentVelocity.magnitude;
+
+		gameObject.rigidbody2D.velocity = targetVelocity;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		GameState gameState = GameObject.Find("camera").GetComponent<GameState>();
+		gameState.thrower = this;
 	}
 }
