7dedc55a5cbf274ff7f06681b6f8c2055b45756c
Jan Hackl <jan@brokenrul.es>
2014-12-07 03:50:54 +0100

    tried to get the controls on keyboard, but too many buttons at the same time :(

--- Interception/Assets/Player.cs
+++ Interception/Assets/Player.cs
@@ -22,15 +22,10 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
-		inputVector.x = Input.GetAxis("Horizontal P" + nr);
-		inputVector.y = -Input.GetAxis("Vertical P" + nr);
-
-//		print("x " + inputVector.x + " y " + inputVector.y);
+		inputVector.x = Input.GetAxis("P" + nr + " Horizontal");
+		inputVector.y = Input.GetAxis("P" + nr + " Vertical");
 
 		Vector2 targetVelocity = (inputVector.magnitude == 0) ? Vector2.zero : inputVector.normalized * Mathf.Min(inputVector.magnitude, 1f) * speed;
-
-//		Vector2 currentVelocity = rigidbody2D.velocity;
-//		float currentSpeed = currentVelocity.magnitude;
 
 		gameObject.rigidbody2D.velocity = targetVelocity;
 	}
